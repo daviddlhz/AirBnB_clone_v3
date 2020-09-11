fe9ee382e44c0e914404cf1ff45be0a2bcaf3a39
daviddlhz <vidda1959@gmail.com>
2020-09-10 23:56:31 -0500

    Fix the problem

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,17 +2,16 @@
 """
 Flask web application api
 """
-from flask import Blueprint
-from flask import Flask, jsonify
-from flask_cors import CORS
+from flask import Flask, Blueprint, jsonify, make_response
 from models import storage
 from api.v1.views import app_views
 import os
+from flask_cors import CORS
 
 app = Flask(__name__)
 cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
-app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
-app.register_blueprint(app_views, url_prefix='/api/v1')
+
+app.register_blueprint(app_views)
 
 
 @app.teardown_appcontext
@@ -23,11 +22,15 @@ def teardown_db(exception):
 
 @app.errorhandler(404)
 def page_not_found(e):
-    """set the 404 status explicitly"""
-    return jsonify({"error": "Not found"}), 404
+    """not found page 404"""
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 
-if __name__ == '__main__':
-    my_host = os.getenv('HBNB_API_HOST')
-    my_port = os.getenv('HBNB_API_PORT')
-    app.run(host=my_host, port=my_port, threaded=True)
+@app.errorhandler(400)
+def not_json(e):
+    """note that we set the 404 status explicitly"""
+    return make_response("Not a JSON", 400)
+
+if __name__ == "__main__":
+    app.run(host=os.getenv('HBNB_API_HOST', '0.0.0.0'),
+            port=int(os.getenv('HBNB_API_PORT', '5000')))
